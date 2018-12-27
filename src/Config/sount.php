8990532adc1ba189c3f6f595ee7d0ee0a51e055d
xiaolin <462441355@qq.com>
2018-12-27 15:51:45 +0800

    * 过来特殊符号

--- src/Config/sount.php
+++ src/Config/sount.php
@@ -9,11 +9,30 @@
 return [
     'elasticsearch' => [
         'prefix' => env('ELASTICSEARCH_PREFIX', 'laravel_'),
-        'index' => env('ELASTICSEARCH_INDEX', 'laravel'),
         'hosts' => [
             env('ELASTICSEARCH_HOST', 'http://localhost'),
         ],
         'analyzer' => env('ELASTICSEARCH_ANALYZER', 'ik_max_word'),
-        'settings' => []
+        'settings' => [],
+        'filter' => [
+            '+',
+            '-',
+            '&',
+            '|',
+            '!',
+            '(',
+            ')',
+            '{',
+            '}',
+            '[',
+            ']',
+            '^',
+            '\\',
+            '"',
+            '~',
+            '*',
+            '?',
+            ':'
+        ]
     ],
 ];
